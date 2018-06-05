64d8988a0d9316ee23bac658401cc837c5a74f7e
Tzu Ming Harry Hsu <stmharry@mit.edu>
2018-06-05 15:43:56 -0400

    Reorder functions

--- bulb/net.py
+++ bulb/net.py
@@ -1,12 +1,9 @@
-import abc
 import torch
 
 from bulb.saver import Saver
 
 
 class Net(object):
-    __metaclass__ = abc.ABCMeta
-
     def __init__(self,
                  model,
                  data_loader=None,
@@ -15,41 +12,47 @@ class Net(object):
 
         self.model = model
         self.writer = writer
-        self.data_loader = self.data_loader
+        self.data_loader = data_loader
 
         self.num_epoch = 0
 
-        self.init(**kwargs)
-
-    def init(self):
-        pass
+        self._init(**kwargs)
 
     def _register_vars(self, vars_dict):
         for (var_name, var) in vars_dict.items():
             setattr(self, var_name, var)
 
-    def pre_batch(self):
-        pass
+    def _prepare_vars(self, requires_grad):
+        for var_name in self._var_names:
+            var = getattr(self, var_name)
+            var = torch.tensor(var, requires_grad=requires_grad).cuda()
+            setattr(self, var_name, var)
 
-    @abc.abstractmethod
-    def step_batch(self):
-        pass
+    def _prepare_losses(self):
+        for loss_name in self._loss_names:
+            loss = getattr(self, loss_name)
+            setattr(self, loss_name, loss.item())
 
-    def post_batch(self):
-        pass
-
-    def log(self):
+    def _log(self):
         strings = [
             '[{:s}]'.format(self.name),
             'epoch: {:d}'.format(self.num_epoch + 1),
             'batch: {:d}/{:d}'.format(self.num_batch + 1, len(self.data_loader)),
-            'loss: {:.4f}'.format(self.loss.item()),
+            'loss: {:.4f}'.format(self.loss),
         ]
 
         print('\t'.join(strings))
 
-    def pre_epoch(self):
-        pass
+    def _summarize(self):
+        for loss_name in self._loss_names:
+            loss = getattr(self, loss_name)
+
+            if self.writer is not None:
+                self.writer.add_scalar(
+                    '{:s}/{:s}'.format(self.name, loss_name),
+                    loss,
+                    self.num_step,
+                )
 
     def step_epoch(self, num_step=None):
         self.pre_epoch()
@@ -63,32 +66,28 @@ class Net(object):
             self.pre_batch()
 
             losses_dict = self.step_batch()
-            losses_dict['total_loss'] = sum(losses_dict.values())
+            losses_dict['loss'] = sum(losses_dict.values())
 
             self._loss_names = losses_dict.keys()
             self._register_vars(losses_dict)
 
             self.post_batch()
-            self.log()
 
         self.post_epoch()
 
         self.num_epoch += 1
 
-    def post_epoch(self):
-        pass
-
 
 class TrainMixin(object):
     name = 'train'
 
-    def init(self,
-             lr,
-             lr_decay_epochs,
-             lr_decay_rate,
-             summarize_steps,
-             save_steps,
-             saver):
+    def _init(self,
+              lr,
+              lr_decay_epochs,
+              lr_decay_rate,
+              summarize_steps,
+              save_steps,
+              saver):
 
         self.lr = lr
         self.lr_decay_epochs = lr_decay_epochs
@@ -103,16 +102,10 @@ class TrainMixin(object):
         else:
             self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=self.lr_decay_epochs, gamma=self.lr_decay_rate)
 
-    def summarize(self):
-        for loss_name in self._loss_names:
-            loss = getattr(self, loss_name)
-
-            if self.writer is not None:
-                self.writer.add_scalar(
-                    '{:s}/{:s}'.format(self.name, loss_name),
-                    loss.item(),
-                    self.num_step,
-                )
+    def _optimize(self):
+        self.optimizer.zero_grad()
+        self.loss.backward()
+        self.optimizer.step()
 
     def save(self):
         state_dict = {
@@ -126,26 +119,23 @@ class TrainMixin(object):
         self.model.load_state_dict(state_dict['model'])
         self.optimizer.load_state_dict(state_dict['optimizer'])
 
-    def pre_epoch(self):
-        self.model.train()
-
     def pre_batch(self):
-        for var_name in self._var_names:
-            var = getattr(self, var_name)
-            if not isinstance(var, list):
-                var = torch.Tensor(var, requires_grad=True).cuda()
-                setattr(self, var_name, var)
+        self._prepare_vars(requires_grad=True)
 
     def post_batch(self):
-        self.optimizer.zero_grad()
-        self.loss.backward()
-        self.optimizer.step()
+        self._optimize()
+        self._prepare_losses()
+
+        self._log()
 
         if (self.num_step % self.summarize_steps == 0):
-            self.summarize()
+            self._summarize()
 
         if (self.num_step % self.save_steps == 0):
             self.save()
+
+    def pre_epoch(self):
+        self.model.train()
 
     def post_epoch(self):
         if self.scheduler is not None:
@@ -162,41 +152,33 @@ class TrainMixin(object):
 class TestMixin(object):
     name = 'test'
 
-    def summarize(self):
-        for loss_name in self._loss_names:
-            loss = self._cum_losses[loss_name] / len(self.data_loader)
-
-            if self.writer is not None:
-                self.writer.add_scalar(
-                    '{:s}/{:s}'.format(self.name, loss_name),
-                    loss,
-                    self.num_step,
-                )
+    def _init(self):
+        pass
 
     def load(self, ckpt_dir=None):
         state_dict = Saver.load_model(ckpt_dir=ckpt_dir)
         self.model.load_state_dict(state_dict['model'])
 
-    def pre_epoch(self):
-        self.model.eval()
-        self._cum_losses = {}
-
     def pre_batch(self):
-        for var_name in self._var_names:
-            var = getattr(self, var_name)
-            if not isinstance(var, list):
-                var = torch.Tensor(var).cuda()
-                setattr(self, var_name, var)
+        self._prepare_vars(requires_grad=False)
 
     def post_batch(self):
+        self._prepare_losses()
+
         for loss_name in self._loss_names:
-            if loss_name in self._cum_losses:
-                _cum_loss = self._cum_losses[loss_name]
+            if loss_name in self._losses:
+                _loss = self._losses[loss_name]
             else:
-                _cum_loss = 0.
+                _loss = 0.
 
             loss = getattr(self, loss_name)
-            self._cum_losses[loss_name] = _cum_loss + loss.item()
+            self._losses[loss_name] = _loss + (loss - _loss) / (self.num_batch + 1)
+
+        self._log()
+
+    def pre_epoch(self):
+        self.model.eval()
+        self._losses = {}
 
     def post_epoch(self):
-        self.summarize()
+        self._summarize()
